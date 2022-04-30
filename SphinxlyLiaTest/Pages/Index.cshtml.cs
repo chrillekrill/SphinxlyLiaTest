4ee910874dec2dd2befa4d45e4cfb6e668cfb5a3
Christoffer K <henricson21@hotmail.com>
2022-04-30 21:42:05 +0200

    first commit

---
+++ SphinxlyLiaTest/Pages/Index.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace SphinxlyLiaTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public bool IsLoggedIn { get; set; }
+        [BindProperty]
+        public string? LoginName { get; set; }
+
+
+        public void OnGet(string loginName, bool isLoggedIn = false)
+        {
+            this.LoginName = loginName;
+
+            IsLoggedIn = isLoggedIn;
+
+            if(isLoggedIn)
+            {      
+                if(Request.Cookies["UserCookie"] != null)
+                {
+                    if (!Request.Cookies["UserCookie"].Equals(loginName))
+                    {
+                        CreateNewUser();
+                    }
+                    else
+                    {
+                        ViewData["Returning"] = "Välkommen tillbaka! Du är inloggad som";
+                        ViewData["UserName"] = LoginName;
+                    }
+                } else
+                {
+                    CreateNewUser();
+                }
+      
+            } 
+        }
+
+        private void CreateNewUser()
+        {
+            ViewData["Returning"] = "Du är inloggad som";
+            Response.Cookies.Append("UserCookie", LoginName);
+            ViewData["UserName"] = LoginName;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("Index", new { loginName = LoginName, isLoggedIn = true });
+            }
+
+            IsLoggedIn = false;
+
+            return Page();
+        }
+    }
+}
---
+++ SphinxlyLiaTest/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const modal = document.getElementById("loginModal");
+
+const modalLoginButton = document.getElementById("btn");
+
+const container = document.getElementById("container");
+const header = document.getElementById("header");
+
+const close = document.getElementsByClassName("closeButton")[0];
+
+modalLoginButton.addEventListener("click", () => {
+    modal.classList.toggle('visible');
+    container.classList.toggle('blur');
+    container.classList.toggle('open');
+    header.classList.toggle('blur');
+});
+
+close.addEventListener("click", () => {
+    modal.classList.toggle('visible');
+    container.classList.toggle('blur');
+    container.classList.toggle('open');
+    header.classList.toggle('blur');
+});
